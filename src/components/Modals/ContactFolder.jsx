131b7b40104a05b75b1136f4d1b8d872d766a0b5
mooon-dev <zainab.42847@gmail.com>
2025-02-11 12:04:38 +0600

    updated contact

--- src/components/Modals/ContactFolder.jsx
+++ src/components/Modals/ContactFolder.jsx
@@ -1,4 +1,86 @@
-// components/Modals/ContactFolder.js
+// // components/Modals/ContactFolder.js
+// import React from "react";
+// import Draggable from "react-draggable";
+
+// // Import your social media logo images and the info.txt icon.
+// import instagram from "../../assets/instagram.png";
+// import xLogo from "../../assets/x.png"; // For Twitter (X)
+// import thread from "../../assets/thread.png";
+// import github from "../../assets/github.png";
+// import mastodon from "../../assets/mastodon.png";
+// import bluesky from "../../assets/bluesky.png";
+// import proton from "../../assets/proton.png";
+// import discord from "../../assets/discord.png";
+// // import linkedin from "../../assets/linkedin.png";
+// // import youtube from "../../assets/youtube.png";
+// import textFileImage from "../../assets/txtfile.png";
+
+// const ContactFolder = ({ setOpenProject }) => {
+//   // Array of social media items with name, logo, and link.
+//   const socialMedia = [
+//     { name: "Instagram", logo: instagram, url: "https://www.instagram.com/cx7.here/" },
+//     { name: "X", logo: xLogo, url: "https://x.com/cx7here" },
+//     { name: "Thread", logo: thread, url: "https://www.threads.net/@cx7.here?hl=en" },
+//     { name: "GitHub", logo: github, url: "https://github.com/xcx-7" },
+//     { name: "Mastodon", logo: mastodon, url: "https://mastodon.social/@cx7" },
+//     { name: "Bluesky", logo: bluesky, url: "https://bsky.app/profile/cx-7.bsky.social" },
+//     { name: "Proton Mail", logo: proton, url: "cxe-7@proton.me" },
+//     { name: "Discord", logo: discord, url: "https://discord.gg/yKJhF5HSrm" },
+
+
+//     // { name: "LinkedIn", logo: linkedin, url: "https://www.linkedin.com/in/yourprofile" },
+//     // { name: "YouTube", logo: youtube, url: "https://www.youtube.com/yourchannel" },
+//   ];
+
+//   return (
+//     <Draggable>
+//       <div className="absolute top-1/4 left-1/4 bg-gray-800 p-4 rounded-lg shadow-lg w-96 h-auto">
+//         {/* Title Bar */}
+//         <div className="flex justify-between items-center border-b pb-2">
+//           <span className="text-white font-semibold">Contacts</span>
+//           <button
+//             className="text-white bg-red-500 px-2 py-1 rounded hover:bg-red-600"
+//             onClick={() => setOpenProject(null)}
+//           >
+//             ✖
+//           </button>
+//         </div>
+
+//         {/* Social Media Icons + Info.txt */}
+//         <div className="mt-4 grid grid-cols-3 gap-4">
+//           {socialMedia.map((item, index) => (
+//             <div
+//               key={index}
+//               className="flex flex-col items-center cursor-pointer"
+//               onClick={() => window.open(item.url, "_blank")}
+//             >
+//               <img src={item.logo} alt={item.name} className="w-16 h-16" />
+//               <span className="text-white text-sm">{item.name}</span>
+//             </div>
+//           ))}
+
+//           {/* Info.txt File */}
+//           <div
+//             className="flex flex-col items-center cursor-pointer"
+//             onDoubleClick={() => setOpenProject(null)}
+//           >
+//             <img src={textFileImage} alt="Info.txt" className="w-16 h-16" />
+//             <span className="text-white text-sm">Info.txt</span>
+//           </div>
+//         </div>
+//       </div>
+//     </Draggable>
+//   );
+// };
+
+// export default ContactFolder;
+
+
+
+
+
+
+
 import React from "react";
 import Draggable from "react-draggable";
 
@@ -11,8 +93,6 @@ import mastodon from "../../assets/mastodon.png";
 import bluesky from "../../assets/bluesky.png";
 import proton from "../../assets/proton.png";
 import discord from "../../assets/discord.png";
-// import linkedin from "../../assets/linkedin.png";
-// import youtube from "../../assets/youtube.png";
 import textFileImage from "../../assets/txtfile.png";
 
 const ContactFolder = ({ setOpenProject }) => {
@@ -26,14 +106,11 @@ const ContactFolder = ({ setOpenProject }) => {
     { name: "Bluesky", logo: bluesky, url: "https://bsky.app/profile/cx-7.bsky.social" },
     { name: "Proton Mail", logo: proton, url: "cxe-7@proton.me" },
     { name: "Discord", logo: discord, url: "https://discord.gg/yKJhF5HSrm" },
-
-
-    // { name: "LinkedIn", logo: linkedin, url: "https://www.linkedin.com/in/yourprofile" },
-    // { name: "YouTube", logo: youtube, url: "https://www.youtube.com/yourchannel" },
   ];
 
   return (
-    <Draggable>
+    // Add cancel=".draggable-ignore" so clicks on elements with that class won’t trigger dragging.
+    <Draggable cancel=".draggable-ignore">
       <div className="absolute top-1/4 left-1/4 bg-gray-800 p-4 rounded-lg shadow-lg w-96 h-auto">
         {/* Title Bar */}
         <div className="flex justify-between items-center border-b pb-2">
@@ -61,8 +138,11 @@ const ContactFolder = ({ setOpenProject }) => {
 
           {/* Info.txt File */}
           <div
-            className="flex flex-col items-center cursor-pointer"
-            onDoubleClick={() => setOpenProject(null)}
+            className="draggable-ignore flex flex-col items-center cursor-pointer"
+            onDoubleClick={() => {
+              console.log("Info.txt double-clicked");
+              setOpenProject("ContactInfo");
+            }}
           >
             <img src={textFileImage} alt="Info.txt" className="w-16 h-16" />
             <span className="text-white text-sm">Info.txt</span>
