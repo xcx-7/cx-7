bd505b2e73bea1b42d368c0a9069977b46300762
mooon-dev <zainab.42847@gmail.com>
2025-02-18 14:17:27 +0600

    updated

--- src/components/Modals/AboutModal.jsx
+++ src/components/Modals/AboutModal.jsx
@@ -1,36 +1,4 @@
-// // components/Modals/AboutModal.js
-// import React from "react";
-// import Draggable from "react-draggable";
 
-// const AboutModal = ({ setOpenProject }) => {
-//   return (
-//     <Draggable>
-//       <div className="absolute top-1/4 left-1/4 bg-gray-900 p-4 rounded-lg shadow-lg w-96 h-72">
-//         <div className="flex justify-between items-center border-b pb-2">
-//           <span className="text-white font-semibold">About</span>
-//           <button
-//             className="text-white bg-red-500 px-2 py-1 rounded hover:bg-red-600"
-//             onClick={() => setOpenProject(null)}
-//           >
-//             ✖
-//           </button>
-//         </div>
-//         <div className="mt-4 text-white text-sm">
-//           This is a custom desktop environment built using React. Enjoy
-//           customizing your OS and exploring the features!
-//         </div>
-//       </div>
-//     </Draggable>
-//   );
-// };
-
-// export default AboutModal;
-
-
-
-
-
-// components/Modals/AboutFileModal.js
 import React from "react";
 import Draggable from "react-draggable";
 
